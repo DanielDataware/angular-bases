117601a57f451929e3ab820b0aef0ce7557bc3ec
Daniel Alvarado <md.alvarado@dataware.com.mx>
2023-10-30 23:57:19 -0600

    fin de seccion 4

---
+++ src/app/counter/components/counter/counter.component.ts
@@ -0,0 +1,45 @@
+// import {Component} from"@angular/core"
+
+// @Component({
+//   selector: 'app-counter',
+//   template: '<h1>Hola Counter</h1>'
+// })
+// export class CounterComponent{
+
+// }//CounterComponent
+
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-counter',
+  template:  `
+  <h3>Counter: {{counter}}</h3>
+<button (click)="increaseBy(1)">+1</button>
+<button (click)="resetCounter()">Reset</button>
+<button (click)="decreaseBy(1)">-1</button>
+  `
+})
+
+export class CounterComponent{
+
+    counter : number  = 10
+
+    increaseBy(value: number):void{
+
+      this.counter+=1
+
+    }
+    decreaseBy(value: number):void{
+
+      this.counter-=1
+
+    }
+
+    resetCounter():void{
+      this.counter = 10
+    }
+
+
+}
+
+
